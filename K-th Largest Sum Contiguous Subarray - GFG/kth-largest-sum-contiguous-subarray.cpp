d488c0eea592f901cc876b99c71de9c7be064192
Abhishek0421 <98973585+Abhishek0421@users.noreply.github.com>
2022-06-12 16:36:58 +0530

    Added solution - LeetHub

--- K-th Largest Sum Contiguous Subarray - GFG/kth-largest-sum-contiguous-subarray.cpp
+++ K-th Largest Sum Contiguous Subarray - GFG/kth-largest-sum-contiguous-subarray.cpp
@@ -10,17 +10,19 @@ using namespace std;
 class Solution{
 public:
     int kthLargest(vector<int> &Arr,int N,int K){
-        vector<int> v;
+        priority_queue<int,vector<int>,greater<int>> pq;
         for(int i=0;i<N;i++){
             int sum=0;
             for(int j=i;j<N;j++){
                 sum+=Arr[j];
-               // cout<<sum<<" ";
-                v.push_back(sum);
+                pq.push(sum);
+                if(pq.size()>K){
+                    pq.pop();
+                }
             }
         }
-        sort(v.begin(),v.end(),greater<int>());
-        return v[K-1];
+        
+        return pq.top();
     }
 };
 
