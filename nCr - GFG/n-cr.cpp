f63366cd5dcfd018647aadce2429eb9d789f77df
Abhishek0421 <98973585+Abhishek0421@users.noreply.github.com>
2022-06-20 08:39:16 +0530

    Added solution - LeetHub

--- nCr - GFG/n-cr.cpp
+++ nCr - GFG/n-cr.cpp
@@ -9,43 +9,24 @@ using namespace std;
 
 class Solution{
 public:
-    int mod = 1e9+7;
-    // int nCr(int n, int r){
-    //     // code here
-    //     if(n<r) return 0;
-    //     //long long int fac=0;
-    //     int dp[n+1][r+2];
-    //     for(int i=0;i<=n;i++){
-    //         dp[i][0]=1;
-    //     }
-    //     dp[0][1]=0;
-    //     for(int i=1;i<=n;i++){
-    //         for(int j=1;j<=i+1;j++){
-    //             dp[i][j] = (dp[i-1][j]+dp[i-1][j-1])%mod;
-    //         }
-    //     }
-    //     return dp[n][r];
-    // }
     int nCr(int n, int r)
    {
-       int dp[n+1][n+1];
-       if(n<r){
+       if(r>n){
            return 0;
        }
-       for(int i=0;i<=n;i++)
-         dp[i][0]=1;
-         
-         for(int i=0;i<=n;i++){
-             for(int j=0;j<i+1;j++){
-                 if(j==0 || i==j){
-                     dp[i][j]=1;
-                 }
-                 else{
-                     dp[i][j] = ((dp[i-1][j-1])%1000000007+(dp[i-1][j])%1000000007)%1000000007;
-                }
-             }
-         }
-         return dp[n][r];
+       if(r>(n-r)){
+           r=n-r;
+       }
+       int mod=1e9+7;
+       int dp[r+1];
+       memset(dp,0,sizeof(dp));
+       dp[0]=1;
+       for(int i=1;i<=n;i++){
+           for(int j=min(r,i);j>0;j--){
+               dp[j]=(dp[j-1]+dp[j])%mod;
+           }
+       }
+       return dp[r];
    }
 };
 
