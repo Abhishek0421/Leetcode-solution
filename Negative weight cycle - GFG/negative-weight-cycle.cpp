50335f1d30c7a4e2d161bc2086987006339188da
Abhishek0421 <98973585+Abhishek0421@users.noreply.github.com>
2022-06-23 17:56:23 +0530

    Added solution - LeetHub

--- Negative weight cycle - GFG/negative-weight-cycle.cpp
+++ Negative weight cycle - GFG/negative-weight-cycle.cpp
@@ -8,6 +8,7 @@ public:
 	int isNegativeWeightCycle(int n, vector<vector<int>>edges){
 	    // Code her
 	    vector<int> dis(n,1e9);
+	    dis[0]=0;
 	    for(int i=0;i<n-1;i++){
 	        for(auto x:edges){
 	            int u = x[0];
