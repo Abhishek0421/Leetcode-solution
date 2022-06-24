5c01ada92e8aa2bf76408b59a4637b9dc76cfdb6
Abhishek0421 <98973585+Abhishek0421@users.noreply.github.com>
2022-06-24 14:03:40 +0530

    Added solution - LeetHub

--- Painting the Fence - GFG/painting-the-fence.cpp
+++ Painting the Fence - GFG/painting-the-fence.cpp
@@ -7,39 +7,35 @@ using namespace std;
  // } Driver Code Ends
 class Solution{
     public:
-    long long  dp[5001][3];
-    long long solve(int prevcol,int i,int prev,int n,int k){
+    int dp[5001][3];
+    int solve(int n,int k,int i,int prevcol,int count){
         if(i>n){
             return 1;
         }
-        if(dp[i][prev]!=-1){
-            return dp[i][prev];
+        if(dp[i][count]!=-1){
+            return dp[i][count];
         }
-        long long ans = 0;
+        int ans = 0;
         for(int j=1;j<=k;j++){
-            if(prev==2 && prevcol==j){
-                continue;
+            if(prevcol==j && count==2){
+               continue;
             }
             else{
                 if(prevcol==j){
-                    ans+=solve(j,i+1,prev+1,n,k);
+                    ans = ans + solve(n,k,i+1,j,count+1);
                 }
                 else{
-                    ans+=solve(j,i+1,1,n,k);
+                    ans = ans + solve(n,k,i+1,j,1);
                 }
                 ans%=1000000007;
             }
         }
-        
-        return dp[i][prev] = ans;
+        return dp[i][count]=ans;
     }
     long long countWays(int n, int k){
         // code here
         memset(dp,-1,sizeof(dp));
-       // vector<int> col(k+1,0);
-        return solve(0,1,0,n,k);
-       
-        //return 0;
+        return solve(n,k,1,0,0);
     }
 };
 
