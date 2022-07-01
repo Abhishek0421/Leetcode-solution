9580fd5c620148a3a8bb095931144281d2eaef08
Abhishek0421 <98973585+Abhishek0421@users.noreply.github.com>
2022-07-01 14:02:27 +0530

    Added solution - LeetHub

--- Find median in a stream - GFG/find-median-in-a-stream.cpp
+++ Find median in a stream - GFG/find-median-in-a-stream.cpp
@@ -9,53 +9,38 @@ class Solution
 {
     public:
     //Function to insert heap.
-    priority_queue<int,vector<int>,greater<int>> p2;
-    priority_queue<int> p1;
+    priority_queue <int> max_heap;
+    priority_queue <int , vector<int>,greater<int>> min_heap;
+    
     void insertHeap(int &x)
     {
-        if(p1.empty() || p1.top()>=x){
-            p1.push(x);
-        }
-        else{
-            p2.push(x);
-        }
+        max_heap.push(x);
+        min_heap.push(max_heap.top());
+        max_heap.pop();
         balanceHeaps();
     }
     
     //Function to balance heaps.
     void balanceHeaps()
     {
-        int diff = p1.size()-p2.size();
-        if(abs(diff)>1){
-            if(p1.size()>p2.size()){
-                p2.push(p1.top());
-                p1.pop();
-            }
-            else{
-                p1.push(p2.top());
-                p2.pop();
-            }
+        if(min_heap.size()>max_heap.size())
+        {
+            max_heap.push(min_heap.top());
+            min_heap.pop();
         }
     }
     
     //Function to return Median.
     double getMedian()
     {
-        int ans=0;
-        if(p1.size()==0 && p2.size()==0){
-            return -1;
+        double ans=0;
+        if((min_heap.size()+max_heap.size())%2!=0) 
+        {
+            ans=max_heap.top();
         }
-        if(p1.size()==p2.size()){
-             ans = ((double)p1.top()+p2.top())/2;
-           // return ans;
-        }
-        else {
-            if(p1.size()>p2.size()){
-                ans = p1.top();
-            }
-            else{
-                ans = p2.top();
-            }
+        else
+        {    ans=max_heap.top()+min_heap.top();
+             ans/=2;
         }
         return ans;
     }
