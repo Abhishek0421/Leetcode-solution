65cda5771ede585c70aba71e33c78e6beeb506d8
Abhishek0421 <98973585+Abhishek0421@users.noreply.github.com>
2022-07-01 15:48:23 +0530

    Added solution - LeetHub

--- Floyd Warshall - GFG/floyd-warshall.cpp
+++ Floyd Warshall - GFG/floyd-warshall.cpp
@@ -16,8 +16,8 @@ class Solution {
 	        for(int i=0; i<n; i++) {
 	            for(int j=0; j<n; j++) {
 	                if(matrix[i][k]==-1 || matrix[k][j]==-1) continue;
-	                if(matrix[i][j]==-1) matrix[i][j] = matrix[i][k] + matrix[k][j];
-	                else matrix[i][j] = min(matrix[i][j], matrix[i][k]+matrix[k][j]);
+	                if(matrix[i][j]==-1) matrix[i][j] = matrix[i][k]+matrix[k][j];
+	                else matrix[i][j] = min(matrix[i][j],matrix[i][k]+matrix[k][j]);
 	            }
 	        }
 	    }
