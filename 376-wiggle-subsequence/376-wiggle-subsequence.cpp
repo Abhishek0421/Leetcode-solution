2f1a95174b9e5a9625e91aa5c54bfd7d7b0bb7b6
Abhishek0421 <98973585+Abhishek0421@users.noreply.github.com>
2022-07-03 10:11:06 +0530

    Time: 64 ms (5.09%), Space: 20.3 MB (5.01%) - LeetHub

---
+++ 376-wiggle-subsequence/376-wiggle-subsequence.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    int dp[1001][1001][3];
+    int solve(vector<int> &num,int i,int j,int c){
+        if(j==num.size() || i==num.size()){
+            return 0;
+        }
+        if(dp[i][j][c]!=-1){
+            return dp[i][j][c];
+        }
+        int ans = 0;
+        if(c==0){
+            if(num[i]-num[j]<0){
+                ans =max(1+solve(num,j,j+1,2),solve(num,i,j+1,0));
+            }
+            else if(num[i]-num[j]>0){
+                ans =max(1+solve(num,j,j+1,1),solve(num,i,j+1,0));
+            }
+            else{
+                ans = solve(num,i,j+1,0);
+                //ans = max(solve(num,i,j+1,0),solve(num,i+1,j+1,0));
+            }
+        }
+        else if(c==2){
+            if(num[i]-num[j]>0){
+                ans = max(1+solve(num,j,j+1,1),solve(num,i,j+1,2));
+            }
+            else{
+                ans = solve(num,i,j+1,2);
+                //ans = max(solve(num,i,j+1,0),solve(num,i+1,j+1,0));
+            }
+        }
+        else{
+            if(num[i]-num[j]<0){
+                ans = max(1+solve(num,j,j+1,2),solve(num,i,j+1,1));
+            }
+            else{
+                ans = solve(num,i,j+1,1);
+            }
+        }
+        return dp[i][j][c]=ans;
+    }
+    int wiggleMaxLength(vector<int>& nums) {
+        memset(dp,-1,sizeof(dp));
+        int n = nums.size();
+        if(n==1){
+            return 1;
+        }
+        return solve(nums,0,1,0)+1;
+    }
+};
