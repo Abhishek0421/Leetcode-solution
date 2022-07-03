95d7b25f893daacbf9b6d19a914f1b2549386a7b
Abhishek0421 <98973585+Abhishek0421@users.noreply.github.com>
2022-07-03 10:12:37 +0530

    Time: 95 ms (5.09%), Space: 20.4 MB (5.01%) - LeetHub

--- 376-wiggle-subsequence/376-wiggle-subsequence.cpp
+++ 376-wiggle-subsequence/376-wiggle-subsequence.cpp
@@ -17,8 +17,8 @@ public:
                 ans =max(1+solve(num,j,j+1,1),solve(num,i,j+1,0));
             }
             else{
-                ans = solve(num,i,j+1,0);
-                //ans = max(solve(num,i,j+1,0),solve(num,i+1,j+1,0));
+                //ans = solve(num,i,j+1,0);
+                ans = max(solve(num,i,j+1,0),solve(num,i+1,j+1,0));
             }
         }
         else if(c==2){
@@ -26,8 +26,8 @@ public:
                 ans = max(1+solve(num,j,j+1,1),solve(num,i,j+1,2));
             }
             else{
-                ans = solve(num,i,j+1,2);
-                //ans = max(solve(num,i,j+1,0),solve(num,i+1,j+1,0));
+                //ans = solve(num,i,j+1,2);
+                ans = max(solve(num,i,j+1,2),solve(num,i+1,j+1,2));
             }
         }
         else{
@@ -35,7 +35,8 @@ public:
                 ans = max(1+solve(num,j,j+1,2),solve(num,i,j+1,1));
             }
             else{
-                ans = solve(num,i,j+1,1);
+                //ans = solve(num,i,j+1,1);
+                ans = max(solve(num,i,j+1,1),solve(num,i+1,j+1,1));
             }
         }
         return dp[i][j][c]=ans;
