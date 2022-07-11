5b80091249bebd34458b0555b36e3999d2becbfb
Abhishek0421 <98973585+Abhishek0421@users.noreply.github.com>
2022-07-11 17:52:54 +0530

    Added solution - LeetHub

--- Geek and his Binary Strings - GFG/geek-and-his-binary-strings.cpp
+++ Geek and his Binary Strings - GFG/geek-and-his-binary-strings.cpp
@@ -17,8 +17,8 @@ class Solution{
     int mod=1000000007;
     for(int i=2;i<=N;i++){
         t[i]=0;
-        for(int j=0;j<i;j++){
-            t[i]=(t[i]% mod)+((t[j]% mod)*(t[i-j-1]% mod)) % mod;
+        for(int j=1;j<=i;j++){
+            t[i]=(t[i]% mod)+((t[j-1]% mod)*(t[i-j]% mod)) % mod;
         }
     }
     return t[N]%mod;
