cb782d52eaadfd8f631d884941338f7d0e72e2a0
Abhishek0421 <98973585+Abhishek0421@users.noreply.github.com>
2022-07-11 17:30:52 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (88.50%) - LeetHub

--- 96-unique-binary-search-trees/96-unique-binary-search-trees.cpp
+++ 96-unique-binary-search-trees/96-unique-binary-search-trees.cpp
@@ -7,8 +7,8 @@ public:
         dp[1]=1;
         for(int i =2;i<=n;i++){
             int sum = 0;
-            for(int j=1;j<=i;j++)
-                sum += dp[j-1]*dp[i-j];
+            for(int j=0;j<i;j++)
+                sum += dp[j]*dp[i-j-1];
             dp[i] = sum;
         }
         return dp[n];
