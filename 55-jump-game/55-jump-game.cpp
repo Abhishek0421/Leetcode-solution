c4a5730b8b5b49a5db9b27605774a8b48621725f
Abhishek0421 <98973585+Abhishek0421@users.noreply.github.com>
2022-07-14 09:09:29 +0530

    Time: 93 ms (65.77%), Space: 48.3 MB (93.28%) - LeetHub

---
+++ 55-jump-game/55-jump-game.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n = nums.size();
+        if(n==1){
+            return true;
+        }
+        int steps = nums[0];
+        
+        int i=0;
+        while(i<n-1){
+            if(steps==0){
+            return false;
+            }
+            steps--;
+            i++;
+            
+            if(steps < nums[i]){
+                steps = nums[i];
+            }
+            // if(steps == 0){
+            //     return false;
+            // }
+        }
+        return true;
+    }
+};
