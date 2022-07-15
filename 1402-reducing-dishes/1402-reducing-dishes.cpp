6d7fa770bca34450b7ca039c75c0727d1431abe6
Abhishek0421 <98973585+Abhishek0421@users.noreply.github.com>
2022-07-15 09:25:33 +0530

    Time: 7 ms (67.55%), Space: 8.2 MB (44.05%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.cpp
+++ 1402-reducing-dishes/1402-reducing-dishes.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
-    int dp[501][501];
-    int solve(vector<int> &s,int i,int t){
-        if(i>=s.size()){
-            return 0;
+    int maxSatisfaction(vector<int>& S) {
+        sort(S.begin(),S.end(),greater<int>());
+        int sum=0;
+        vector<int> P;
+        for(int i=0;i<S.size();i++){
+            sum+=S[i];
+            if(sum<0) break;
+            P.push_back(S[i]);
         }
-        if(dp[i][t]!=-1){
-            return dp[i][t];
+        reverse(P.begin(),P.end());
+        sum=0;
+        for(int i=0;i<P.size();i++){
+            sum+=(i+1)*P[i];
         }
-        return dp[i][t] = max((t+1)*s[i] + solve(s,i+1,t+1),solve(s,i+1,t));
-    }
-    int maxSatisfaction(vector<int>& satisfaction) {
-        memset(dp,-1,sizeof(dp));
-        sort(satisfaction.begin(),satisfaction.end());
-        return solve(satisfaction,0,0);
+        return sum;
     }
 };
