d5a288ac24211018cf4090b09f1e2ce109a7f6c4
Abhishek0421 <98973585+Abhishek0421@users.noreply.github.com>
2022-11-20 11:43:24 +0530

    Added solution - LeetHub

--- Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
+++ Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
@@ -1,66 +1,67 @@
-// { Driver Code Starts
+//{ Driver Code Starts
 #include<bits/stdc++.h>
 using namespace std;
 
- // } Driver Code Ends
-
-
+// } Driver Code Ends
 class Solution
 {
 	public:
 	//Function to find sum of weights of edges of the Minimum Spanning Tree.
-    int par(int u,vector<int> &parent){
-        if(parent[u]==u){
-            return u;
-        }
-        return parent[u] = par(parent[u],parent);
-    }
-    void mst(int x,int y,vector<int> &rank,vector<int> &parent){
-        x = par(x,parent);
-        y = par(y,parent);
-        if(rank[x]>rank[y]){
-            parent[y]=x;
-        }
-        else if(rank[x]<rank[y]){
-            parent[x]=y;
-        }
-        else{
-            parent[x]=y;
-            rank[y]++;
-        }
-    }
+	int par(int u,vector<int> &parent){
+	    if(parent[u]==u){
+	        return u;
+	    }
+	    return parent[u] = par(parent[u],parent);
+	}
+	void mst(int u,int v,vector<int> &rank,vector<int> &parent){
+	    u = par(u,parent);
+	    v = par(v,parent);
+	    
+	    if(rank[u]>rank[v]){
+	        parent[v] = u; 
+	    }
+	    else if(rank[v]>rank[u]){
+	        parent[v]=u;
+	    }
+	    else{
+	        parent[u]=v;
+	        rank[v]++;
+	    }
+	}
     int spanningTree(int V, vector<vector<int>> adj[])
     {
         // code here
-        vector<int> parent(V);
+        int ans =0;
+        vector<int> dis;
+        vector<int> rank(V,0);
+        vector<int> parent(V,-1);
         for(int i=0;i<V;i++){
             parent[i]=i;
         }
-        vector<int> rank(V,0);
-        int sum=0;
-        set<pair<int,pair<int,int> > > st;
+        set<pair<int,pair<int,int> >> st;
         for(int i=0;i<V;i++){
             for(auto x:adj[i]){
                 st.insert({x[1],{i,x[0]}});
             }
         }
+        
         while(!st.empty()){
             auto it = *st.begin();
             st.erase(st.begin());
             int d = it.first;
-            int x = it.second.first;
-            int y = it.second.second;
-            if(par(x,parent)!=par(y,parent)){
-                sum+=d;
-              //  cout<<x<<" "<<y<<"\n";
-                mst(x,y,rank,parent);
+            int u = it.second.first;
+            int v = it.second.second;
+            
+            if(par(u,parent)!=par(v,parent)){
+                mst(u,v,rank,parent);
+                ans+=d;
             }
         }
-        return sum;
+        return ans;
     }
 };
 
-// { Driver Code Starts.
+//{ Driver Code Starts.
 
 
 int main()
@@ -91,4 +92,5 @@ int main()
     return 0;
 }
 
-  // } Driver Code Ends
+
+// } Driver Code Ends
